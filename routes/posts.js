72ed15a8bd456811cb7e467a861488b061256219
Jollen Chen <jollen@moko365.com>
2014-11-23 11:28:24 +0800

    update, #4

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,16 @@
+var post = 
+[{
+	"title": "您好",
+	"content": "今天星期日"
+},
+{
+	"title": "Hello",
+	"content": "Today is Sunday"
+}];
+
+
 exports.list = function(req, res){
+	res.send(post);
 };
 
 exports.create = function(req, res){
